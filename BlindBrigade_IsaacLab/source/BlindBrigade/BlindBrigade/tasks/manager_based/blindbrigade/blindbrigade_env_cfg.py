78168c5972ccb9c61c8e7f02f78c51703b7d3742
JagtapSagar <jagtapsagar247@gmail.com>
2025-12-23 04:49:36 -0600

    Precudural terrain with flat path locations for robot reset

--- BlindBrigade_IsaacLab/source/BlindBrigade/BlindBrigade/tasks/manager_based/blindbrigade/blindbrigade_env_cfg.py
+++ BlindBrigade_IsaacLab/source/BlindBrigade/BlindBrigade/tasks/manager_based/blindbrigade/blindbrigade_env_cfg.py
@@ -16,52 +16,143 @@ from isaaclab.managers import SceneEntityCfg
 from isaaclab.managers import TerminationTermCfg as DoneTerm
 from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.utils import configclass
+from isaaclab.terrains import (
+    TerrainImporterCfg,
+    TerrainGeneratorCfg,
+    MeshRepeatedBoxesTerrainCfg,
+    MeshPlaneTerrainCfg,
+    FlatPatchSamplingCfg
+)
+from isaaclab.sensors import RayCasterCfg, patterns
 
 from . import mdp
 
 ##
 # Pre-defined configs
 ##
+# from scipy.spatial.transform import Rotation as R
+from isaaclab.envs.mdp.commands import UniformPose2dCommandCfg
+from wheeledlab_assets.mushr import MUSHR_SUS_CFG
+from wheeledlab_tasks.common import Mushr4WDActionCfg
+from wheeledlab.envs.mdp.observations import root_euler_xyz
+from .mdp.events import reset_root_state
 
-from isaaclab_assets.robots.cartpole import CARTPOLE_CFG  # isort:skip
-
+import torch
+from isaaclab.envs import ManagerBasedEnv
 
 ##
 # Scene definition
 ##
 
-
 @configclass
 class BlindbrigadeSceneCfg(InteractiveSceneCfg):
     """Configuration for a cart-pole scene."""
 
-    # ground plane
-    ground = AssetBaseCfg(
+    terrain = TerrainImporterCfg(
         prim_path="/World/ground",
-        spawn=sim_utils.GroundPlaneCfg(size=(100.0, 100.0)),
+        terrain_type="generator",
+        max_init_terrain_level=None,
+        terrain_generator=TerrainGeneratorCfg(
+            size=(8.0, 8.0),
+            border_width=20.0,
+            horizontal_scale=0.1,
+            vertical_scale=0.005,
+            slope_threshold=0.75,
+            difficulty_range=(0.0, 1.0),
+            use_cache=False,
+            sub_terrains={
+                # "flat": MeshPlaneTerrainCfg(proportion=0.2),
+                "boxes":MeshRepeatedBoxesTerrainCfg(
+                    platform_width=0,
+                    platform_height=0,
+                    object_params_start=MeshRepeatedBoxesTerrainCfg.ObjectCfg(
+                        num_objects=40, height=0.25, size=(0.05, 0.05), max_yx_angle=0.0, degrees=True
+                    ),
+                    object_params_end=MeshRepeatedBoxesTerrainCfg.ObjectCfg(
+                        num_objects=60, height=0.45, size=(0.3, 0.3), max_yx_angle=60.0, degrees=True
+                    ),
+                    flat_patch_sampling={
+                        "root_blind_spawn": FlatPatchSamplingCfg(
+                            num_patches=1, patch_radius=[0.05, 0.1, 0.2, 0.3, 0.4, 0.5], max_height_diff=0.01),
+                        "root_priv_spawn": FlatPatchSamplingCfg(
+                            num_patches=1, patch_radius=[0.05, 0.1, 0.2, 0.3, 0.4, 0.5], max_height_diff=0.01),
+                        "target_spawn": FlatPatchSamplingCfg(
+                            num_patches=1, patch_radius=[0.05, 0.1, 0.2, 0.3, 0.4, 0.5], max_height_diff=0.01),
+                    },
+                ),
+            },
+        ),
+        collision_group=-1,
+        physics_material=sim_utils.RigidBodyMaterialCfg(
+            friction_combine_mode="multiply",
+            restitution_combine_mode="multiply",
+            static_friction=1.0,
+            dynamic_friction=1.0,
+        ),
+        visual_material=sim_utils.PreviewSurfaceCfg(
+            diffuse_color=(0.0, 0.0, 0.0),
+        ),
+        debug_vis=True,
     )
 
-    # robot
-    robot: ArticulationCfg = CARTPOLE_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
-
-    # lights
-    dome_light = AssetBaseCfg(
-        prim_path="/World/DomeLight",
-        spawn=sim_utils.DomeLightCfg(color=(0.9, 0.9, 0.9), intensity=500.0),
+    light = AssetBaseCfg(
+        prim_path="/World/light",
+        spawn=sim_utils.DistantLightCfg(color=(0.75, 0.75, 0.75), intensity=3000.0),
     )
 
+    robot: ArticulationCfg = MUSHR_SUS_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+    # priv_robot: ArticulationCfg = MUSHR_SUS_CFG.replace(prim_path="{ENV_REGEX_NS}/RobotPriv")
+
+    height_scanner = RayCasterCfg(
+        prim_path="{ENV_REGEX_NS}/Robot/mushr_nano/base_link",
+        offset=RayCasterCfg.OffsetCfg(
+            pos=(0.0, 0.0, 20.0),
+            # rot=(0.0, 1.0, 0.0, 0.0),
+        ),
+        attach_yaw_only=True,
+        pattern_cfg=patterns.GridPatternCfg(size=[100, 100], resolution=0.1),
+        debug_vis=False,
+        mesh_prim_paths=["/World/ground"],
+    )
+
+    def __post_init__(self):
+        """Post intialization."""
+        super().__post_init__()
+        self.robot.init_state = self.robot.init_state.replace(
+            pos=(0.0, 0.0, 0.1)
+        )
+        self.priv_robot.init_state = self.robot.init_state.replace(
+            pos=(0.1, 0.1, 0.1)
+        )
+        self.terrain.terrain_generator.num_rows = int(self.num_envs / 2)
+        self.terrain.terrain_generator.num_cols = int(self.num_envs / 2)
+        self.terrain.terrain_generator.sub_terrains["boxes"].flat_patch_sampling["root_blind_spawn"].num_patches = self.num_envs
+        self.terrain.terrain_generator.sub_terrains["boxes"].flat_patch_sampling["root_priv_spawn"].num_patches = self.num_envs
+        self.terrain.terrain_generator.sub_terrains["boxes"].flat_patch_sampling["target_spawn"].num_patches = self.num_envs
+
+@configclass
+class EventCfg:
+    """Configuration for the events."""
+
+    set_goal = EventTerm(
+        func=reset_root_state,
+        mode="reset",
+        params={
+            "blind_asset_cfg": SceneEntityCfg("robot"),
+            "priv_asset_cfg": SceneEntityCfg("priv"),
+        },
+    )
 
 ##
 # MDP settings
 ##
 
-
-@configclass
-class ActionsCfg:
-    """Action specifications for the MDP."""
-
-    joint_effort = mdp.JointEffortActionCfg(asset_name="robot", joint_names=["slider_to_cart"], scale=100.0)
-
+def goal_relative_xyz(env : ManagerBasedEnv):
+    pos = mdp.root_pos_w(env)
+    goal_pos = mdp.generated_commands(env, "goal_pose")
+    goal_pos = goal_pos[:, :2]  # we only need the x, y coordinates
+    rel_pos = goal_pos - pos[:, :2]
+    return torch.nan_to_num(rel_pos, nan=0)
 
 @configclass
 class ObservationsCfg:
@@ -70,11 +161,18 @@ class ObservationsCfg:
     @configclass
     class PolicyCfg(ObsGroup):
         """Observations for policy group."""
-
-        # observation terms (order preserved)
-        joint_pos_rel = ObsTerm(func=mdp.joint_pos_rel)
-        joint_vel_rel = ObsTerm(func=mdp.joint_vel_rel)
-
+        goal_relative_xyz = ObsTerm(
+            func=goal_relative_xyz,
+        )
+        world_euler_xyz = ObsTerm(
+            func=root_euler_xyz,
+        )
+        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, clip=(-10., 10.))
+        base_ang_vel = ObsTerm(func=mdp.base_ang_vel, clip=(-10., 10.))
+        last_action = ObsTerm(
+            func=mdp.last_action,
+            clip=(-1., 1.)
+        )
         def __post_init__(self) -> None:
             self.enable_corruption = False
             self.concatenate_terms = True
@@ -84,88 +182,55 @@ class ObservationsCfg:
 
 
 @configclass
-class EventCfg:
-    """Configuration for events."""
-
-    # reset
-    reset_cart_position = EventTerm(
-        func=mdp.reset_joints_by_offset,
-        mode="reset",
-        params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]),
-            "position_range": (-1.0, 1.0),
-            "velocity_range": (-0.5, 0.5),
-        },
-    )
-
-    reset_pole_position = EventTerm(
-        func=mdp.reset_joints_by_offset,
-        mode="reset",
-        params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]),
-            "position_range": (-0.25 * math.pi, 0.25 * math.pi),
-            "velocity_range": (-0.25 * math.pi, 0.25 * math.pi),
-        },
-    )
-
-
-@configclass
 class RewardsCfg:
     """Reward terms for the MDP."""
-
-    # (1) Constant running reward
     alive = RewTerm(func=mdp.is_alive, weight=1.0)
-    # (2) Failure penalty
     terminating = RewTerm(func=mdp.is_terminated, weight=-2.0)
-    # (3) Primary task: keep pole upright
-    pole_pos = RewTerm(
-        func=mdp.joint_pos_target_l2,
-        weight=-1.0,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]), "target": 0.0},
-    )
-    # (4) Shaping tasks: lower cart velocity
-    cart_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.01,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"])},
-    )
-    # (5) Shaping tasks: lower pole angular velocity
-    pole_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.005,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"])},
-    )
 
 
 @configclass
 class TerminationsCfg:
     """Termination terms for the MDP."""
-
-    # (1) Time out
     time_out = DoneTerm(func=mdp.time_out, time_out=True)
-    # (2) Cart out of bounds
-    cart_out_of_bounds = DoneTerm(
-        func=mdp.joint_pos_out_of_manual_limit,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]), "bounds": (-3.0, 3.0)},
-    )
-
 
 ##
 # Environment configuration
 ##
 
+@configclass
+class ElevationCommandCfg:
+    """Configuration for the elevation commands."""
+
+    goal_pose = UniformPose2dCommandCfg(
+        asset_name="robot",
+        ranges=UniformPose2dCommandCfg.Ranges(
+            pos_x=(-19.0, 19.0),
+            pos_y=(-19.0, 19.0),
+            heading=(-3.14, 3.14),
+        ),
+        resampling_time_range=(10.0, 10.0),
+        simple_heading=True,
+        debug_vis=True
+    )
+
 
 @configclass
 class BlindbrigadeEnvCfg(ManagerBasedRLEnvCfg):
+
+    seed: int = 42
+
     # Scene settings
-    scene: BlindbrigadeSceneCfg = BlindbrigadeSceneCfg(num_envs=4096, env_spacing=4.0)
+    scene: BlindbrigadeSceneCfg = BlindbrigadeSceneCfg(num_envs=4, env_spacing=0)
+
     # Basic settings
     observations: ObservationsCfg = ObservationsCfg()
-    actions: ActionsCfg = ActionsCfg()
-    events: EventCfg = EventCfg()
+    actions: Mushr4WDActionCfg = Mushr4WDActionCfg()
     # MDP settings
     rewards: RewardsCfg = RewardsCfg()
     terminations: TerminationsCfg = TerminationsCfg()
+
+    commands = ElevationCommandCfg = ElevationCommandCfg()
+    events: EventCfg = EventCfg()
 
     # Post initialization
     def __post_init__(self) -> None:
---
+++ BlindBrigade_IsaacLab/source/BlindBrigade/BlindBrigade/tasks/manager_based/blindbrigade/mdp/events.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+import math
+import re
+import torch
+from typing import TYPE_CHECKING, Literal
+
+import carb
+import omni.physics.tensors.impl.api as physx
+from isaacsim.core.utils.extensions import enable_extension
+from pxr import Gf, Sdf, UsdGeom, Vt
+
+import isaaclab.sim as sim_utils
+import isaaclab.utils.math as math_utils
+from isaaclab.actuators import ImplicitActuator
+from isaaclab.assets import Articulation, DeformableObject, RigidObject
+from isaaclab.managers import EventTermCfg, ManagerTermBase, SceneEntityCfg
+from isaaclab.sim.utils.stage import get_current_stage
+from isaaclab.terrains import TerrainImporter
+from isaaclab.utils.version import compare_versions
+
+if TYPE_CHECKING:
+    from isaaclab.envs import ManagerBasedEnv
+
+
+
+def reset_root_state(
+    env: ManagerBasedEnv,
+    env_ids: torch.Tensor,
+    blind_asset_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
+    priv_asset_cfg: SceneEntityCfg = SceneEntityCfg("priv"),
+):
+    """Reset the asset root state to a random position and velocity uniformly within the given ranges.
+
+    This function randomizes the root position and velocity of the asset.
+
+    * It samples the root position from the given ranges and adds them to the default root position, before setting
+      them into the physics simulation.
+    * It samples the root orientation from the given ranges and sets them into the physics simulation.
+    * It samples the root velocity from the given ranges and sets them into the physics simulation.
+
+    The function takes a dictionary of pose and velocity ranges for each axis and rotation. The keys of the
+    dictionary are ``x``, ``y``, ``z``, ``roll``, ``pitch``, and ``yaw``. The values are tuples of the form
+    ``(min, max)``. If the dictionary does not contain a key, the position or velocity is set to zero for that axis.
+    """
+    # extract the used quantities (to enable type-hinting)
+    asset: RigidObject | Articulation = env.scene[blind_asset_cfg.name]
+    # get default root state
+    root_states = asset.data.default_root_state[env_ids].clone()
+
+    blind_patch_view = env.scene.terrain.flat_patches['root_blind_spawn'].view(-1,3)
+    priv_patch_view = env.scene.terrain.flat_patches['root_priv_spawn'].view(-1,3)
+    goal_patch_view = env.scene.terrain.flat_patches['target_spawn'].view(-1,3)
+
+    select_ids = torch.randint(0, blind_patch_view.shape[0], (len(env_ids),), device=env_ids.device)
+
+    # pos
+    positions = blind_patch_view[select_ids,:]
+    orientations = root_states[:, 3:7]
+    # velocities
+    velocities = root_states[:, 7:13]
+
+    # set into the physics simulation
+    asset.write_root_pose_to_sim(torch.cat([positions, orientations], dim=-1), env_ids=env_ids)
+    asset.write_root_velocity_to_sim(velocities, env_ids=env_ids)
