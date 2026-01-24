7e88a313e463a411b4dc01ce16beb4b61b8d619c
Godwyll Aikins <godwyll.aikins2@uta.edu>
2026-01-24 15:44:10 -0600

    added guide environment

---
+++ BlindBrigade_IsaacLab/source/BlindBrigade/BlindBrigade/tasks/manager_based/guide/agents/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+gym.register(
+    id="Template-guide-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.guide_env_cfg:GuideEnvCfg",
+        "rl_games_cfg_entry_point": f"{__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+        "skrl_cfg_entry_point": f"{__name__}:skrl_ppo_cfg.yaml",
+    },
+)
